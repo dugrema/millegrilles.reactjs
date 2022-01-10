5d702958172b097611ac61b4ad2929252cec6256
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-01-10 18:07:00 -0500

    Comparaison subtle et WASM

--- example/src/HachageWasm.js
+++ example/src/HachageWasm.js
@@ -1,3 +1,4 @@
+// Importer javascript global pour la librairie hash-wasm
 import 'hash-wasm/dist/blake2b.umd.min.js'
 import 'hash-wasm/dist/blake2s.umd.min.js'
 
@@ -5,7 +6,7 @@ import { useEffect, useState } from 'react'
 // import { sha512, blake2b, blake2s } from 'hash-wasm'
 import { createSHA512, createBLAKE2b } from 'hash-wasm'
 import reactjs from '@dugrema/millegrilles.reactjs'
-import { hacher, verifierHachage, encoderIdmg, verifierIdmg } from '@dugrema/millegrilles.utiljs'
+import { hacher, verifierHachage, encoderIdmg, verifierIdmg, Hacheur, VerificateurHachage } from '@dugrema/millegrilles.utiljs'
 
 function HachageWasm(props) {
 
@@ -13,8 +14,8 @@ function HachageWasm(props) {
 
     useEffect(()=>{
         // runHachage(setVal).catch(err=>console.error("Erreur %O", err))
-        // hacherUtiljs(setVal).catch(err=>console.error("Erreur %O", err))
-        hacherCertificat(setVal).catch(err=>console.error("Erreur %O", err))
+        hacherUtiljs(setVal).catch(err=>console.error("Erreur %O", err))
+        // hacherCertificat(setVal).catch(err=>console.error("Erreur %O", err))
     }, [])
 
     return (
@@ -74,10 +75,51 @@ async function hacherUtiljs(setVal) {
     const verifSha512 = await verifierHachage(hachageSha512, int32Buffer)
     console.debug("Verification SHA2-512 : %O", verifSha512)
 
+    let buffer100MB = new Uint8Array(100 * 1024 * 1024) // 25 MB
+    const debut = new Date()
+    const hachageBlake2b100mb = await hacher(buffer100MB, {hashingCode: 'blake2b-512'})
+    const finHachage = new Date()
+    const verifBlake2b100mb = await verifierHachage(hachageBlake2b100mb, buffer100MB)
+    const finVerif = new Date()
+
+    const hachageSha512Subtle = await hacher(buffer100MB, {hashingCode: 'sha2-512'})
+    const finSha512 = new Date()
+    const hash = await window.crypto.subtle.digest('SHA-512', buffer100MB);
+    const finSubtle = new Date()
+
+    const hacheurSha512 = new Hacheur({hashingCode: 'sha2-512'})
+    await hacheurSha512.ready
+    await hacheurSha512.update(buffer100MB)
+    const hachage512WasmUpdate = await hacheurSha512.finalize()
+    const finHacheurUpdate = new Date()
+
+    const verifSha512100mb = await verifierHachage(hachage512WasmUpdate, buffer100MB)
+
+    const verificateurSha512 = new VerificateurHachage(hachage512WasmUpdate)
+    await verificateurSha512.ready
+    await verificateurSha512.update(buffer100MB)
+    const verifSha512100mbWASM = await verificateurSha512.verify()
+
+    buffer100MB = null
+    console.debug("Verification Blake2b 100MB : %O", verifBlake2b100mb)
+    console.debug("Duree hachage : %O", finHachage.getTime() - debut.getTime())
+    console.debug("Duree verification : %O", finVerif.getTime() - finHachage.getTime())
+
+    console.debug("Hachage SHA512 avec auto-detect (subtle) duree : %O, hachage: %O", finSha512.getTime() - finVerif.getTime(), hachageSha512Subtle)
+    console.debug("Hachage SHA512 subtle duree : %O", finSubtle.getTime() - finSha512.getTime())
+    console.debug("Hachage SHA512 WASM duree : %O, resultat : %O", finHacheurUpdate.getTime() - finSubtle.getTime(), hachage512WasmUpdate)
+
     const texte = `
         SHA2-512: ${hachageSha512} (verif ${verifBlake2s})
         BLAKE2b: ${hachageBlake2b} (verif ${verifBlake2b})
         BLAKE2s: ${hachageBlake2s} (verif ${verifSha512})
+
+        Hachage Blake2b 25MB, duree ${finHachage.getTime() - debut.getTime()}
+        ${hachageBlake2b100mb}
+        Verification ${verifBlake2b100mb}, duree ${finVerif.getTime() - finHachage.getTime()}
+
+        Verif 100mb ${hachage512WasmUpdate} 
+        avec wasm ${verifSha512100mb}, WASM ${verifSha512100mbWASM}
     `
     setVal(texte)
 
--- src/hachage.js
+++ src/hachage.js
@@ -12,12 +12,22 @@ const constructeur = async methode => {
     }
 }
 
+// Utilise subtle pour digest() one shot et WASM pour blocks avec update/finalize
+const constructeurSubtle = async (methode, algo) => {
+    const hacheur = await methode()
+    return {
+        update: buffer => hacheur.update(buffer),
+        finalize: () => hacheur.digest('binary'),
+        digest: async buffer => window.crypto.subtle.digest(algo, buffer)
+    }
+}
+
 const hacheurs = {
     // hash-wasm
-    'sha256': () => constructeur(createSHA256),
-    'sha2-256': () => constructeur(createSHA256),
-    'sha512': () => constructeur(createSHA512),
-    'sha2-512': () => constructeur(createSHA512),
+    'sha256': () => constructeurSubtle(createSHA256, 'SHA-256'),
+    'sha2-256': () => constructeurSubtle(createSHA256, 'SHA-256'),
+    'sha512': () => constructeurSubtle(createSHA512, 'SHA-512'),
+    'sha2-512': () => constructeurSubtle(createSHA512, 'SHA-512'),
     'blake2s256': () => constructeur(createBLAKE2s),
     'blake2s-256': () => constructeur(createBLAKE2s),
     'blake2b512': () => constructeur(createBLAKE2b),
