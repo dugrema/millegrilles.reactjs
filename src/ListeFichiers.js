f002495695162ebff215c1b692411fe11e126825
mathieu <mathieu.dugre@mdugre.info>
2023-10-15 16:26:28 -0400

    WIP refact mobile

--- src/ListeFichiers.js
+++ src/ListeFichiers.js
@@ -12,19 +12,23 @@ const MIMETYPE_PDF = 'application/pdf'
 const TOUCH_ENABLED = isTouchEnabled()  // Detecter touch (appareil mobile)
 
 export function ListeFichiers(props) {
+    // Selectionner mode de liste (desktop ou mobile) selon presence des evenements touch
+    if(TOUCH_ENABLED) return ListeFichiersMobile(props)
+    return ListeFichiersDesktop(props)
+}
+
+function ListeFichiersDesktop(props) {
 
     // Intercepter onClick pour capturer la selection
     const { 
         modeView, rows, isListeComplete, colonnes, modeSelectionActif, selection, 
         onContextMenu, onSelect, scrollValue, 
         onScroll, onOpen, onClickEntete,
-        suivantCb,
+        suivantCb, capabilities,
     } = props
 
     const [idxSelectionShift, setIdxSelectionShift] = useState(0)
-    const [touchEvent, setTouchEvent] = useState('')
     const [scrollRestored, setScrollRestored] = useState(false)
-    const [longTouchTimer, setLongTouchTimer] = useState('')
 
     const contextMenuHandler = useCallback( async e => {
         e.stopPropagation()
@@ -38,6 +42,119 @@ export function ListeFichiers(props) {
         }
         if(onContextMenu) onContextMenu(e, value)
     }, [onContextMenu, onSelect, selection, rows, onSelect])
+
+    const ouvrirItemHandler = useCallback(e=>{
+        const { idx, value } = e.currentTarget.dataset
+        setIdxSelectionShift(idx)
+        if(onSelect) onSelect([value])
+        if(onOpen) onOpen(rows[idx])
+    }, [rows, setIdxSelectionShift, onSelect, onOpen])
+
+    const selectionToggleHandler = useCallback(e=>{
+        const {shiftKey, ctrlKey} = e
+        const { idx, value } = e.currentTarget.dataset
+        // console.debug("Selection handler idx:%O, value:%O, ctrl:%s, shift:%s", idx, value, ctrlKey, shiftKey)
+
+        // Mode selection - soit touche CTRL (PC) ou toggle sur mobile
+        const modeSelection = ctrlKey || modeSelectionActif
+
+        if(shiftKey) {
+            // Retirer selection texte
+            const selection = window.getSelection()
+            selection.removeAllRanges()
+        }
+
+        // Determiner comportement cles speciales
+        if(shiftKey && !selection.includes(value)) {
+            // Selectionner un range
+            const debut = Math.min(idx, idxSelectionShift),
+                  fin = Math.max(idx, idxSelectionShift)
+
+            // console.debug("Ajouter range %d -> %d (liste %O)", debut, fin, rows)
+            
+            const idMapperFct = colonnes.idMapper || idMapper
+            const selectionMaj = [...selection]
+            for(let i=debut; i<=fin; i++) {
+                const valueAdd = idMapperFct(rows[i])
+                // console.debug("Ajouter idx:%s : %s", i, valueAdd)
+                if(!selectionMaj.includes(valueAdd)) selectionMaj.push(valueAdd)
+            }
+
+            if(onSelect) onSelect(selectionMaj)
+
+        } else if(modeSelection) {
+            if(!shiftKey) setIdxSelectionShift(idx)
+
+            // Reset selection cumulee
+            if( selection.includes(value) ) {
+                // Deselectionner
+                const selectionMaj = selection.filter(item=>item!==value)
+                if(onSelect) onSelect(selectionMaj)
+            } else {
+                // Selectionner
+                if(onSelect) onSelect([...selection, value])
+            }
+
+        } else {
+            setIdxSelectionShift(idx)
+            if(onSelect) onSelect([value])
+        }
+
+    }, [rows, selection, idxSelectionShift, modeSelectionActif, onSelect, setIdxSelectionShift])
+
+    const eventHandlers = useMemo(()=>{
+        // PC
+        return {
+            onClick: selectionToggleHandler, 
+            onDoubleClick: ouvrirItemHandler,
+            onContextMenu: contextMenuHandler,
+        }
+    }, [selectionToggleHandler, ouvrirItemHandler])
+
+    useEffect(()=>{
+        if(scrollRestored) return
+        setScrollRestored(true)
+        if(scrollValue > 0) window.scroll({top: scrollValue})
+    }, [scrollValue, scrollRestored, setScrollRestored])
+
+    useEffect(() => {
+        const onScrollHandler = (e) => {
+            const pos = e.target.documentElement.scrollTop
+            if(onScroll) onScroll(pos)
+        };
+        window.addEventListener('scroll', onScrollHandler);
+        return () => window.removeEventListener('scroll', onScrollHandler);
+    }, [onScroll])
+
+    return (
+        <div>
+            <ListeFichiersItemsDesktop 
+                modeView={modeView}
+                rows={rows} 
+                isListeComplete={isListeComplete}
+                colonnes={colonnes}
+                selection={selection}
+                eventHandlers={eventHandlers} 
+                onClickEntete={onClickEntete}
+                suivantCb={suivantCb} />
+        </div>
+    )
+}
+
+function ListeFichiersMobile(props) {
+
+    // Intercepter onClick pour capturer la selection
+    const { 
+        modeView, rows, isListeComplete, colonnes, modeSelectionActif, selection, 
+        onContextMenu, onSelect, scrollValue, 
+        onScroll, onOpen, onClickEntete,
+        suivantCb, capabilities,
+    } = props
+
+    const [idxSelectionShift, setIdxSelectionShift] = useState(0)
+    const [touchEvent, setTouchEvent] = useState('')
+    const [scrollRestored, setScrollRestored] = useState(false)
+    const [longTouchTimer, setLongTouchTimer] = useState('')
 
     const ouvrirItemHandler = useCallback(e=>{
         const { idx, value } = e.currentTarget.dataset
@@ -167,23 +284,15 @@ export function ListeFichiers(props) {
             }
             setLongTouchTimer('')
         }
-    }, [modeSelectionActif, touchEvent, setTouchEvent, ouvrirItemHandler, selectionToggleHandler, onSelect, longTouchTimer, setLongTouchTimer])
+    }, [modeSelectionActif, touchEvent, setTouchEvent, ouvrirItemHandler, selectionToggleHandler, 
+        onSelect, longTouchTimer, setLongTouchTimer])
 
     const eventHandlers = useMemo(()=>{
-        if(TOUCH_ENABLED) {
-            // Mobile
-            return {
-                onTouchStart: touchStartHandler,
-                onTouchMove: touchMoveHandler,
-                onTouchEnd: touchEndHandler,
-            }
-        } else {
-            // PC
-            return {
-                onClick: selectionToggleHandler, 
-                onDoubleClick: ouvrirItemHandler,
-                onContextMenu: contextMenuHandler,
-            }
+        // Mobile
+        return {
+            onTouchStart: touchStartHandler,
+            onTouchMove: touchMoveHandler,
+            onTouchEnd: touchEndHandler,
         }
     }, [selectionToggleHandler, ouvrirItemHandler, touchStartHandler, touchMoveHandler, touchEndHandler])
 
@@ -205,21 +314,86 @@ export function ListeFichiers(props) {
 
     return (
         <div>
-        <ListeFichiersItems 
-            modeView={modeView}
-            rows={rows} 
-            isListeComplete={isListeComplete}
-            colonnes={colonnes}
-            selection={selection}
-            eventHandlers={eventHandlers} 
-            onClickEntete={onClickEntete}
-            suivantCb={suivantCb} />
+            <ListeFichiersItemsMobile 
+                modeView={modeView}
+                rows={rows} 
+                isListeComplete={isListeComplete}
+                colonnes={colonnes}
+                selection={selection}
+                eventHandlers={eventHandlers} 
+                onClickEntete={onClickEntete}
+                suivantCb={suivantCb} />
         </div>
     )
 }
 
 /** Liste de fichiers avec details sur lignes, colonnes configurables */
-function ListeFichiersItems(props) {
+function ListeFichiersItemsDesktop(props) {
+
+    // console.debug("!!! ListeFichiersItems proppies ", props)
+
+    const {
+        modeView, colonnes, rows, 
+        eventHandlers, 
+        isListeComplete, suivantCb,
+        onClickEntete,
+    } = props
+
+    if(!colonnes || !rows) return ''  // Ecran n'est pas encore configure
+
+    const selection = props.selection || []
+
+    const idMapperFct = colonnes.idMapper || idMapper
+
+    const labelFooter = useMemo(()=>{
+        if(!rows) return ''
+        if(rows.length === 0 && isListeComplete) return (
+            <span>Collection vide</span>
+        )
+        if(rows.length === 1) return (
+            <span>{isListeComplete?'Liste complete, ':''} 1 item affiche</span>
+        )
+        return (
+            <span>{isListeComplete?'Liste complete, ':''} {rows.length} items affiches</span>
+        )
+    }, [isListeComplete, rows])
+
+    return (
+        <div className='fichierstable'>
+
+            <ListeFichiersEntete colonnes={colonnes} onClickEntete={onClickEntete} />
+
+            {rows.map((row, idx)=>{
+                const localId = idMapperFct(row, idx)
+                const selected = selection.includes(localId)
+                return (
+                    <FichierItem
+                        key={''+localId} 
+                        modeView={modeView}
+                        eventHandlers={eventHandlers}
+                        colonnes={colonnes} 
+                        data={row}
+                        selected={selected}
+                        idx={idx}
+                        value={localId}
+                        >
+                        
+                    </FichierItem>
+                )
+            })}
+
+            {isListeComplete?'':
+                <BoutonSuivantListe suivantCb={suivantCb} />
+            }
+
+            <p className='listefichiers-footer'>{labelFooter}</p>
+
+        </div>
+    )
+}
+
+/** Liste de fichiers avec details sur lignes, colonnes configurables */
+function ListeFichiersItemsMobile(props) {
 
     // console.debug("!!! ListeFichiersItems proppies ", props)
 
@@ -371,7 +545,8 @@ function FichierItem(props) {
             case 'thumbnails': return FichierThumbnail
             case 'thumbnails-small': return FichierThumbnail
             case 'recents': return ListeFichiersRecents
-            default: 
+            default:
+                if(TOUCH_ENABLED) return ListeFichiersRowMobile
                 return ListeFichiersRow
         }
     }, [modeView])
@@ -389,9 +564,9 @@ function FichierItem(props) {
     }, [dataRow, selected, rowClassname])
 
     // Thumbnails
-    const thumbnail = dataRow.thumbnail || {},
-          {thumbnailIcon, thumbnailSrc} = thumbnail
-    const thumbnailLoader = dataRow.imageLoader
+    // const thumbnail = dataRow.thumbnail || {},
+    //       {thumbnailIcon, thumbnailSrc} = thumbnail
+    // const thumbnailLoader = dataRow.imageLoader
 
     // Convertir data avec rowLoader au besoin
     useEffect(()=>{
@@ -431,6 +606,8 @@ function FichierItem(props) {
 function ListeFichiersRow(props) {
 
     const { selected, idx, value, data, eventHandlers } = props
+
+    console.debug("ListeFichiersRow proppies : ", props)
 
     const colonnes = props.colonnes || {}
     const { rowClassname } = colonnes || {}
@@ -506,6 +683,98 @@ function ListeFichiersRow(props) {
                     </Col>
                 )
             })}
+        </Row>
+    )
+}
+
+function ThumbnailMobile(props) {
+    if(props.src || props.loader) {
+        return <Thumbnail src={props.src} loader={props.loader} small={true} />
+    } else if(props.icon) {
+        return <div className='thumbnailsmall'>{props.icon}</div>
+    } else {
+        return <div className='thumbnailsmall'></div>
+    }
+
+}
+
+function ListeFichiersRowMobile(props) {
+
+    const { selected, idx, value, data, eventHandlers } = props
+
+    console.debug("ListeFichiersRowMobile proppies ", props)
+
+    const colonnes = props.colonnes || {}
+    const { rowClassname } = colonnes || {}
+    const paramsColonnes = colonnes.paramsColonnesMobile || colonnes.paramsColonnes || {}
+    const ordreColonnes = colonnes.ordreColonnesMobile || colonnes.ordreColonnes || {}
+
+    const fichierDesactive = !!data.disabled
+
+    const classNames = useMemo(()=>{
+        let classNames = ['fichierstablerow']
+        if(selected) classNames.push('selectionne')
+        if(rowClassname) {
+            const rowClasses = rowClassname(data)
+            if(rowClasses) {
+                classNames = [...classNames, ...rowClasses.split(' ')]
+            }
+        }
+        return classNames
+    }, [data, selected, rowClassname])
+
+    // Thumbnails
+    const thumbnail = data.thumbnail || {},
+          {thumbnailIcon, thumbnailSrc} = thumbnail
+    const thumbnailLoader = data.thumbnailLoader
+
+    return (
+        <Row 
+            className={classNames.join(' ')}
+            {...eventHandlers}
+            data-idx={idx}
+            data-value={value}
+        >
+            <Col xs={3} md={2}>
+                <ThumbnailMobile src={thumbnailSrc} loader={thumbnailLoader} icon={thumbnailIcon} />
+            </Col>
+
+            <Col xs={9} md={10}>
+                <Row>
+                    {ordreColonnes.map(nomColonne=>{
+                        const param = paramsColonnes[nomColonne]
+                        const classNameCol = param.className || ''
+                        const className = classNameCol || param.className
+                        const Formatteur = param.formatteur
+
+                        const contenu = data[nomColonne] || ''
+                        let spanContenu = <span title={contenu}>{contenu}</span>
+                        if(Formatteur) {
+                            spanContenu = <span><Formatteur value={contenu} data={data} /></span>
+                        }
+
+                        const infoDimension = {
+                            xs: param.xs,
+                            sm: param.sm,
+                            md: param.md,
+                            lg: param.lg,
+                            xl: param.xl,
+                            xxl: param.xxl,
+                        }
+
+                        return (
+                            <Col 
+                                key={nomColonne} 
+                                {...infoDimension} 
+                                className={className}
+                            >
+                                {spanContenu}
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </Col>
+
         </Row>
     )
 }
